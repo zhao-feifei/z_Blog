5c070a12cd49c31bdb8348c887663de95d42faf5
zhao-feifei <398983914@qq.com>
2022-07-03 16:22:12 +0800

    登录弹窗结构完成

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,11 +1,64 @@
+import { useState } from 'react';
+import styles from './index.module/scss';
+
 interface IProps {
   isShow: boolean;
   onClose: Function;
 }
 
 const Login = (props: IProps) => {
-  console.log(props);
   const { isShow = false } = props;
-  return isShow ? <div>登录弹框</div> : null;
+  const [form, setForm] = useState({
+    phone: '',
+    verify: '',
+  });
+  console.log(setForm);
+
+  const handleClose = () => {};
+  const handleGetVerifyCode = () => {};
+  const handleLogin = () => {};
+  const handleOAuthGithub = () => {};
+
+  return isShow ? (
+    <div className={styles.loginArea}>
+      <div className={styles.loginBox}>
+        <div className={styles.loginTitle}>
+          <div>手机号登录</div>
+          <div className={styles.close} onClick={handleClose}>
+            x
+          </div>
+          <input
+            name="phone"
+            type="text"
+            placeholder="请输入手机号"
+            value={form.phone}
+          />
+          <div className={styles.verifyCodeArea}>
+            <input
+              name="verify"
+              type="text"
+              placeholder="请输入验证码"
+              value={form.verify}
+            />
+            <span className={styles.verifyCode} onClick={handleGetVerifyCode}>
+              请输入验证码
+            </span>
+          </div>
+          <div className={styles.loginBtn} onClick={handleLogin}>
+            登录
+          </div>
+          <div className={styles.otherLogin} onClick={handleOAuthGithub}>
+            使用github登录
+          </div>
+          <div className={styles.loginPrivacy}>
+            注册登录即表示同意
+            <a href="https://github.com/" target="_blank" rel="noreferrer">
+              隐私政策
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 export default Login;
