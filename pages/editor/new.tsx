ad10f94f27557d3c0eecfad54d3f0f77d3e45e02
zhao-feifei <398983914@qq.com>
2022-07-08 21:32:36 +0800

    文章编辑样式完成

--- pages/editor/new.tsx
+++ pages/editor/new.tsx
@@ -2,17 +2,30 @@ import '@uiw/react-md-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
 import dynamic from 'next/dynamic';
 import { useState } from 'react';
-import { styles } from './index.module.sass';
+import  styles  from './index.module.scss';
+import { Input ,Button } from 'antd';
 
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
 function NewEditor() {
-  const [value, setValue] = useState('**Hello world!!!**');
+  const [content, setContent] = useState('**Hello world!!!**');
+  const [title,setTitle]=useState('')
+  const handlePublish=()=>{
+
+  }
+  const handleTitleChange=(event)=>{
+    setTitle(event?.target?.value)
+  }
+
   return (
     <div className={styles.container}>
-      <MDEditor value={value} onChange={setValue} />
+      <div className={styles.operation}>
+        <Input className={styles.title} placeholder="请输入文章标题" value={title}onChange={handleTitleChange}></Input>
+        <Button className={styles.button} type="primary" onClick={handlePublish}>发布</Button>
+      </div>
+      <MDEditor value={content} height={1080} onChange={setContent} />
     </div>
   );
 }
-NewEditor.layout = null;
+(NewEditor as any).layout = null;
 export default NewEditor;
